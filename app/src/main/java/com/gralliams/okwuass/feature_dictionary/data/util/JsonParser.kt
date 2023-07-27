cb6f1c99845a4959655326388aed14bf484595af
Great Grant <greatgrant116@gmail.com>
2023-07-27 15:40:11 +0100

    create json parser interface

---
+++ app/src/main/java/com/gralliams/okwuass/feature_dictionary/data/util/JsonParser.kt
@@ -0,0 +1,9 @@
+package com.gralliams.okwuass.feature_dictionary.data.util
+
+import java.lang.reflect.Type
+
+interface JsonParser {
+
+    fun <T> fromJson(json: String, type: Type): T?
+    fun <T> toJson(obj: T, type: Type): String?
+}
