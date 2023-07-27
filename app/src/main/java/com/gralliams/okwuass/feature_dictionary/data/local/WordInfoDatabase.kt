3b447d3c23b7a1f6267812c23af7c7cc9a2b33c9
Great Grant <greatgrant116@gmail.com>
2023-07-27 15:24:14 +0100

    create database class

---
+++ app/src/main/java/com/gralliams/okwuass/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -0,0 +1,12 @@
+package com.gralliams.okwuass.feature_dictionary.data.local
+
+import androidx.room.Database
+import com.gralliams.okwuass.feature_dictionary.data.local.entity.WordInfoEntity
+
+@Database(
+    entities = [WordInfoEntity::class],
+    version = 1
+)
+abstract class WordInfoDatabase {
+    abstract val dao: WordInfoDao
+}
