7d8bf95d025a13aeb51067335d9627ce48b3c5dc
Great Grant <greatgrant116@gmail.com>
2023-07-28 18:06:22 +0100

    create viewmodel

---
+++ app/src/main/java/com/gralliams/okwuass/feature_dictionary/presentation/WordInfoViewModel.kt
@@ -0,0 +1,14 @@
+package com.gralliams.okwuass.feature_dictionary.presentation
+
+import androidx.lifecycle.ViewModel
+import com.gralliams.okwuass.feature_dictionary.domain.use_case.GetWordInfo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+//Inject use case
+@HiltViewModel
+class WordInfoViewModel @Inject constructor (
+    private val getWordInfo: GetWordInfo
+        ): ViewModel(){
+
+}
